b01012da76bc79595567f758f6616e4d1531c876
sam kim <seungkim11@gmail.com>
2016-05-12 04:13:23 -0700

    added instruction page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //By PBR, Seung Kim
 // Group 11
 // Communication Server For Green Corp
-// note: To make contribution easy for others with little experience in NodeJS, all of the logic in this server is implemented in this single file.
+// Please feel free to tag your name and contribute
 
 var express = require('./express');
 var app = express();
@@ -10,12 +10,12 @@ var mongojs = require('mongojs')
 var db = mongojs('green_corp', ['global']);
 // var collection = db.collection('global');
 
-// JSON obj of a maptile
+// // JSON obj of a maptile. must be in ALL CAPS, as in enum value
 //     {
 //         "x":12,
 //         "y":14,
-//         "terrain": "sand", // gravel, soild, rock,
-//         "science": "crystal",  //organic, radioactive, mineral
+//         "terrain": "SAND", // GRAVEL, SOIL, ROCK, SAND, NONE
+//         "science": "CRYSTAL",  // RADIOACTIVE, ORGANIC, MINERAL, ARTIFACT, CRYSTAL, NONE
 //     };
 
 var global = [];
@@ -26,45 +26,98 @@ app.get('/', function (req, res) {
     res.sendFile('index.html');
 });
 
-
 // get Global Map
 app.get('/api/global', function (req, res) {
+    mapToGlobal();
+    res.send(global);
+    global = [];
+});
+
+// convert map object to global array
+function mapToGlobal() {
     var key;
     for (key in map) {
         if (map.hasOwnProperty(key)) {
             global.push(map[key]);
         }
     }
-    res.send(global);
-    global = [];
-});
+}
 
 // post to global map
 app.post('/api/global', function (req, res) {
-
     var tiles = req.body;
 
+    // validate that the data is an array
+    if (tiles && tiles.constructor === Array) {
+        tiles.forEach(function (tile) {
+
+            // validate that data has all attributes and is in correct format
+            if (validateTile(tile)) {
+                var key = tile.x + "/" + tile.y;
+                map[key] = tile;
+            }
+        })
+        res.send('OK');
+    } else {
+        res.send('Data must be an array');
+    }
     // for each tile, save to an object mapping by a key string, to minimize access time
-    tiles.forEach(function (tile) {
-        var key = tile.x + "/" + tile.y;
-        map[key] = tile;
-    })
-    res.send('OK');
+
 });
 
-app.get('/api/global/erase', function (req, res) {
+// resets the data
+app.get('/api/global/reset', function (req, res) {
     global = [];
     map = {};
-    res.send("Data Erased");
+    res.send("Ready for new a game with clean data! Check '/api/global' or '/api/global/size'");
 });
 
+// for debugging
 app.get('/api/debug/mapping', function (req, res) {
     res.send(map)
 });
 
-app.get('/api/debug/global', function (req, res) {
-    res.send(global)
+// for getting the size of the json array
+app.get('/api/global/size', function (req, res) {
+    mapToGlobal();
+    var size = global.length;
+    res.send(size.toString());
+    global = [];
 });
+
+// for tutorial
+app.get('/api/global/test', function (req, res) {
+    var testmap = [
+        {x: 1, y: 2, terrain: "GRAVEL", science: "CRYSTAL"},
+        {x: 3, y: 4, terrain: "SAND", science: "ORGANIC"},
+        {x: 5, y: 6, terrain: "SOIL", science: "MINERAL"},
+        {x: 7, y: 8, terrain: "SOIL", science: "RADIOACTIVE"},
+        {x: 9, y: 10, terrain: "ROCK", science: "ARTIFACT"},
+        {x: 11, y: 12, terrain: "NONE", science: "NONE"}
+    ]
+    res.send(testmap);
+})
+
+// object validator
+function validateTile(tile) {
+    if (!isInt(tile.x) || !isInt(tile.y)) return false;
+
+    // validate science
+    // validate terrain
+
+    return true;
+}
+
+// check if coordinate is an integer
+function isInt(value) {
+    var x;
+    if (isNaN(value)) {
+        return false;
+    }
+    x = parseFloat(value);
+    return (x | 0) === x;
+}
+
 
 // **************** deprecated ***************
 
