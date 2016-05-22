d8c50d7c706552554cc5006a26589eb3e2b73a49
sam kim <seungkim11@gmail.com>
2016-05-22 01:27:46 -0700

    index and instructions update

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/api/global', function (req, res) {
     var rovername = req.header('Rover-Name');
     var secret = req.header('Corp-Secret');
     var rover = rovers[rovername];
-    // console.log("rover " + rover.id + ", secret: " + secret);
+    console.log("rover " + rover.id + ", secret: " + secret);
 
     if (!secret || (secret !== process.env.GREENCORP_537_APIKEY)) {
         res.status(401).send('Unauthorized. You must have GreenCorp secret to post');
@@ -179,6 +179,10 @@ app.post('/api/science/gather/:x/:y', function (req, res) {
 
 app.get('/api/roverinfo', function (req, res) {
     res.send(rovers);
+});
+
+app.get('/*', function (req, res) {
+    res.redirect('/');
 });
 
 app.listen(3000);
